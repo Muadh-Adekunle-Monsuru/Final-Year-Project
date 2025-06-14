c2637f24dc7eef3ebeb0a798adaf43610d0fa10f
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2025-06-14 15:38:26 +0100

    trying middleware

--- fypms/middleware.ts
+++ fypms/middleware.ts
@@ -1,9 +1,14 @@
-import { withAuth } from 'next-auth/middleware';
+// import { withAuth } from 'next-auth/middleware';
 
-export default withAuth({
-	// Matches the pages config in `[...nextauth]`
-	pages: {
-		signIn: '/auth/signin',
-		error: '/error',
-	},
-});
+// export default withAuth({
+// 	// Matches the pages config in `[...nextauth]`
+// 	pages: {
+// 		signIn: '/auth/signin',
+// 	},
+// });
+
+export { default } from 'next-auth/middleware';
+
+export const config = {
+	matcher: ['/trips', '/reservations', '/properties', '/favorites'],
+};
