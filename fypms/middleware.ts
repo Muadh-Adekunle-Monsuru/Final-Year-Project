162c4cec29a27a7a91175c7248b0a61d915e4140
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2025-06-14 15:41:58 +0100

    fix?

--- fypms/middleware.ts
+++ fypms/middleware.ts
@@ -1,14 +1,8 @@
-// import { withAuth } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
 
-// export default withAuth({
-// 	// Matches the pages config in `[...nextauth]`
-// 	pages: {
-// 		signIn: '/auth/signin',
-// 	},
-// });
-
-export { default } from 'next-auth/middleware';
-
-export const config = {
-	matcher: ['/trips', '/reservations', '/properties', '/favorites'],
-};
+export default withAuth({
+	// Matches the pages config in `[...nextauth]`
+	pages: {
+		signIn: '/auth/signin',
+	},
+});
